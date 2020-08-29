d506187cda4a75232a3c94d9a7245bc2df8d2b6b
JacksWebBrand <jbizzell@protonmail.com>
2020-08-29 03:05:39 +0100

    header component, inc logo, nav, links

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+    <Header /> {/*By having header outside the switch below, it will always render, regardless of which page the switch renders */}
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
